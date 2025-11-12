4708634996e0845850744d7705661cd8e9cf490a
BAAH2n <arsen.h.2007@gmail.com>
2025-11-12 21:08:28 +0200

    Refactor SQL to drop dimension tables
    Removed creation of dimension tables for Customer, Product, and Date.

--- Homework/Assignment_3/mart (BigQuery).sql
+++ Homework/Assignment_3/mart (BigQuery).sql
@@ -1,20 +1,4 @@
-DROP TABLE IF EXISTS Assignment_3_dwh.dim_Customer;
-CREATE TABLE Assignment_3_dwh.dim_Customer AS
-SELECT * FROM Assignment_3_dwh.stage_customers;
 
-DROP TABLE IF EXISTS Assignment_3_dwh.dim_Product;
-CREATE TABLE Assignment_3_dwh.dim_Product AS
-SELECT * FROM Assignment_3_dwh.stage_products;
-
-DROP TABLE IF EXISTS Assignment_3_dwh.dim_Date;
-CREATE TABLE Assignment_3_dwh.dim_Date AS
-SELECT
-    CAST(FORMAT_DATE('%Y%m%d', order_date) AS INT64) AS date_id,
-    order_date AS full_date,
-    EXTRACT(DAY FROM order_date) AS day,
-    FORMAT_DATE('%B', order_date) AS month_name,
-    EXTRACT(YEAR FROM order_date) AS year
-FROM Assignment_3_dwh.stage_sales;
 
 
 DROP TABLE IF EXISTS Assignment_3_dwh.fct_Sales;
