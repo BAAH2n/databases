40f76e55177fb67b92374adee05f7f2fd9cdd657
BAAH2n <arsen.h.2007@gmail.com>
2025-11-12 21:08:46 +0200

    Create dimension tables for customers, products, and dates
    Added creation of dimension tables for customers, products, and dates.

--- Homework/Assignment_3/Stage layer (BigQuery).sql
+++ Homework/Assignment_3/Stage layer (BigQuery).sql
@@ -34,3 +34,21 @@ WHERE
     AND customer_user_id IS NOT NULL
     AND product_sku IS NOT NULL
 GROUP BY transaction_id; 
+
+DROP TABLE IF EXISTS Assignment_3_dwh.dim_Customer;
+CREATE TABLE Assignment_3_dwh.dim_Customer AS
+SELECT * FROM Assignment_3_dwh.stage_customers;
+
+DROP TABLE IF EXISTS Assignment_3_dwh.dim_Product;
+CREATE TABLE Assignment_3_dwh.dim_Product AS
+SELECT * FROM Assignment_3_dwh.stage_products;
+
+DROP TABLE IF EXISTS Assignment_3_dwh.dim_Date;
+CREATE TABLE Assignment_3_dwh.dim_Date AS
+SELECT
+    CAST(FORMAT_DATE('%Y%m%d', order_date) AS INT64) AS date_id,
+    order_date AS full_date,
+    EXTRACT(DAY FROM order_date) AS day,
+    FORMAT_DATE('%B', order_date) AS month_name,
+    EXTRACT(YEAR FROM order_date) AS year
+FROM Assignment_3_dwh.stage_sales;
